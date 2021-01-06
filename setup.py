9525141556027a4ec73cf70c855050fef74e209e
Felipe Brum Pereira <felipe.brumpereira@farfetch.com>
2021-01-06 17:04:20 +0000

    chore: Upgrade package version to 0.0.2
    - Add static type checking
    - Add text guard clauses

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requirements = ["flake8==3.7.8", "pytest==5.2.0", "pytest-cov==2.8.1"]
 
 setup(
     name=package_name,
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/Farfetch/checkarg.git",
     license="MIT License (MIT)",
     include_package_data=True,
